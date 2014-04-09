8f84a46fe4e957f652af68ac92650056c9a7653a
Schuyler Erle <schuyler@nocat.net>
2014-04-09 10:55:48 -0700

    Remove fluff from the Ubuntu provisioning script.

--- provision-ubuntu.sh
+++ provision-ubuntu.sh
@@ -1,46 +1,5 @@
 #!/usr/bin/env bash
 
-# i took these from https://github.com/sstephenson/ruby-build/blob/master/bin/ruby-build
-compute_md5() {
-  if type md5 &>/dev/null; then
-    md5 -q
-  elif type openssl &>/dev/null; then
-    local output="$(openssl md5)"
-    echo "${output##* }"
-  elif type md5sum &>/dev/null; then
-    local output="$(md5sum -b)"
-    echo "${output% *}"
-  else
-    return 1
-  fi
-}
-
-verify_checksum() {
-  # If there's no MD5 support, return success
-  [ -n "$HAS_MD5_SUPPORT" ] || return 0
-
-  # If the specified filename doesn't exist, return success
-  local filename="$1"
-  [ -e "$filename" ] || return 0
-
-  # If there's no expected checksum, return success
-  local expected_checksum=`echo "$2" | tr [A-Z] [a-z]`
-  [ -n "$expected_checksum" ] || return 0
-
-  # If the computed checksum is empty, return failure
-  local computed_checksum=`echo "$(compute_md5 < "$filename")" | tr [A-Z] [a-z]`
-  [ -n "$computed_checksum" ] || return 1
-
-  if [ "$expected_checksum" != "$computed_checksum" ]; then
-    { echo
-      echo "checksum mismatch: ${filename} (file is corrupt)"
-      echo "expected $expected_checksum, got $computed_checksum"
-      echo
-    } >&4
-    return 1
-  fi
-}
-
 download_unless_exist() {
   local filename=`basename $1`
   local url="$1"
@@ -51,7 +10,7 @@ download_unless_exist() {
 }
 
 sudo apt-get update
-sudo apt-get install -y build-essential libncurses5-dev openssl libssl-dev git curl libpam0g-dev expect openjdk-7-jdk 
+sudo apt-get install -y openjdk-7-jre-headless
 
 riak_version="2.0.0pre20"
 riak_build="1"
